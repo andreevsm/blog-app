4d45ae4aa68604e336b2cebb24e2a96d047589c1
andreevsm <aandreev.sm@gmail.com>
2021-11-22 23:23:44 +0300

    Fixed mobile sidebar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -19,12 +19,12 @@ const Header = () => {
     <header className="bg-blue left-0 top-0 text-white text-center static w-auto h-auto lg:fixed lg:h-screen lg:w-72 flex lg:block">
       <h1 className="text-2xl font-bold absolute lg:relative w-full lg:w-auto left-0 top-6">Sergey's blog</h1>
 
-      <nav className="flex items-center flex-col px-4 py-8">
-        <button onClick={() => isMenuShownHandler()} type="button" className="relative pl-2 lg:hidden">
+      <nav className="flex flex-col px-4 py-8">
+        <button onClick={() => isMenuShownHandler()} type="button" className="relative w-6 h-6 pl-2 lg:hidden">
           <FaBars size="24" />
         </button>
         {isMenuShown && (
-        <div className="hidden lg:block">
+        <div className="lg:block">
           <div>
             <img src={Avatar} alt="Sergey'blog Avatar" className="w-40 h-40 rounded-full mb-4 mx-auto object-cover" />
             <div className="text-sm mb-4">
